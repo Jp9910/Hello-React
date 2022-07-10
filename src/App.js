cb0ab7ed204c4049c09109fe33a9a9d13f5fe07c
Joao Secundo <alundra@academico.ufs.br>
2022-07-10 18:49:52 -0300

    TextField component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import Banner from './components/Banner/Banner';
+import Banner from './components/Banner/Banner.js'; //".js" (or any file extension) is optional
+//If no file is specified, it tries to import the file named index in the directory
+import TextField from './components/TextField';
 
 function App() {
   return (
     <div className="App">
       <Banner/>
+      <TextField/>
     </div>
   );
 }
---
+++ src/components/TextField/TextField.js
@@ -0,0 +1,11 @@
+import "./TextField.css"
+export const TextField =  () => {
+    return (
+        <div className="text-field">
+            <label>Name</label>
+            <input placeholder="Type your name"/>
+        </div>
+    )
+}
+
+//This file is only the component itself.
