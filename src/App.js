ecbbb07981c528cf17051d6445ecb0679e98c380
Joao Secundo <alundra@academico.ufs.br>
2022-07-10 22:36:13 -0300

    Rename Form and DropdownSelect components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Banner from './components/Banner/Banner.js'; //".js" (or any file extension) is optional
-import Form from './components/Form';
+import Form from './components/Form/Form';
 //If no file is specified, it tries to import the file named index in the directory
 
 function App() {
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,5 @@
 import Button from "../Button/Button"
-import DropdownSelect from "../DropdownSelect"
+import DropdownSelect from "../DropdownSelect/DropdownSelect"
 import TextField from "../TextField"
 import "./Form.css"
 
