f8fbf3638a45953b77b3492be44d84d90dba47db
Joao Secundo <alundra@academico.ufs.br>
2022-07-12 01:39:27 -0300

    Getting the user input

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -1,10 +1,27 @@
 import "./TextField.css"
 export const TextField =  (props) => {
+    
     const placeholderModificada = `${props.placeholder}...` 
+
+    let valor = ""
+    
+    const FunctionAtTypying = (event) => {
+        // The read-only _target_ property of the _Event_ interface is a reference to the object
+        // onto which the event was dispatched. It is different from Event.currentTarget when
+        // the event handler is called during the bubbling or capturing phase of the event. 
+        //console.log(event.target.value)
+
+        valor = event.target.value
+        console.log(valor)
+    }
+
+    // Setting the _value_ attribute in the following component will hard-code it in the component.
+    // This is because React doesn't keep rerendering the component, so the state is immutable.
+    // The only way to reflect a change in state is by (manually rerendering the component?).
     return (
         <div className="text-field">
             <label>{props.label}</label>
-            <input required={props.required} placeholder={placeholderModificada}/>
+            <input value="asdf" onChange={FunctionAtTypying} required={props.required} placeholder={placeholderModificada}/>
         </div>
     )
 }
