91ce0c624faab9c80d4a8e05c1575502c3ca33af
Joao Secundo <alundra@academico.ufs.br>
2022-07-14 04:21:06 -0300

    Team component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Banner from './components/Banner/Banner.js'; //".js" (or any file extension) is optional
 import Form from './components/Form/Form';
+import Team from './components/Team/Team';
+
 //If no file is specified, it tries to import the file named index in the directory
 
 function App() {
@@ -14,11 +16,22 @@ function App() {
     setPlayers([...players, newPlayer])
     console.log("Players registered:" + JSON.stringify(players, null, 4))
   }
+  
+  const teams = [
+    {nome:'DRX', cor1: "#57C278", cor2: "#09F7E9"},
+    {nome:'T1', cor1: "#82CFFA", cor2: "#E8F8FF"},
+    {nome:'LSB', cor1: "#A60157", cor2: "#F0F8E2"},
+  ]
 
   return (
     <div className="App">
       <Banner/>
-      <Form atPlayerRegistered={newPlayer => newPlayerRegistered(newPlayer)}/>
+      <Form atPlayerRegistered={newPlayer => newPlayerRegistered(newPlayer)} teams={teams}/>
+      {/* These 4 next lines are equivalent: */}
+      {teams.map(function(element,index){return <Team key={index} name={element.nome} cor1={element.cor1} cor2={element.cor2} />})}
+      {/*teams.map((element) => {return <Team name="123"/>})*/}
+      {/*teams.map((element) => <Team name="123"/>)*/}
+      {/*teams.map(element => <Team name="123"/>)*/}
     </div>
   );
 }
---
+++ src/components/Team/Team.js
@@ -0,0 +1,11 @@
+import "./Team.css"
+
+const Time = (props) => {
+    return (
+        <section className="team">
+            <h3>{props.name}</h3>
+        </section>
+    )
+}
+
+export default Time
