ed09cef2ed483a766d071db15a12d2377650a19e
Joao Secundo <alundra@academico.ufs.br>
2022-07-14 04:23:42 -0300

    Add style inside the component

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,9 +1,10 @@
 import "./Team.css"
 
 const Time = (props) => {
+    const css = {backgroundColor: props.cor2}
     return (
-        <section className="team">
-            <h3>{props.name}</h3>
+        <section className="team" style={css}>
+            <h3 style={{ borderColor: props.cor1 }}>{props.name}</h3>
         </section>
     )
 }
